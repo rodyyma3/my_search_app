fbdb67f29073469382b35fb761940c249dd58050
rodyyma3 <107746860+rodyyma3@users.noreply.github.com>
2024-05-30 00:07:36 +0900

    ver3

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-from flask import Flask, request, render_template
-import subprocess
 
-app = Flask(__name__)
+#from flask import Flask, request, render_template
+#import subprocess
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+#app = Flask(__name__)
 
-@app.route('/search', methods=['POST'])
-def search():
-    query = request.form['query']
-    result = subprocess.run(['./search.sh', query], capture_output=True, text=True)
-    return f'<pre>{result.stdout}</pre>'
+#@app.route('/')
+#def index():
+    #return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#@app.route('/search', methods=['POST'])
+#def search():
+    #query = request.form['query']
+    #result = subprocess.run(['./search.sh', query], capture_output=True, text=True)
+    #return f'<pre>{result.stdout}</pre>'
+
+#if __name__ == '__main__':
+    #app.run(debug=True)
 
 #---------------------------------------
 
